d7fd5ed13f058bd6caf23f63b6e95199c1b7d6b2
Jason <tian0049@e.ntu.edu.sg>
2016-05-31 13:11:33 -0400

    final

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,3 +1,3 @@
 import collections
-profile_sorted = collections.OrderedDict(sorted(profile, key = lambda x: x[1]))
-profile_sorted = dict(profile_soted)
+profile_sorted = collections.OrderedDict(sorted(profile.items(), key = lambda x: x[1]))
+profile_sorted = dict(profile_sorted)
